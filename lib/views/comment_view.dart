1ce71799d79c1961b837d13431a3d1b3d919f507
KhoaDan9 <khoadangx2@gmail.com>
2024-04-19 17:54:11 +0700

    comment

--- lib/views/comment_view.dart
+++ lib/views/comment_view.dart
@@ -1,7 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:instagramz_flutter/resources/firestore_method.dart';
+import 'package:intl/intl.dart';
 
 class CommentView extends StatefulWidget {
   final String postId;
@@ -31,41 +33,116 @@ class _CommentViewState extends State<CommentView> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      resizeToAvoidBottomInset: true,
-      appBar: AppBar(
-        title: const Text('Comments'),
-        actions: [
-          TextButton(
-            onPressed: () async {
-              await FireStoreMethod()
-                  .storeComment(widget.postId, _textController.text);
-            },
-            child: const Text(
-              'Reply',
-              style: TextStyle(fontSize: 16),
-            ),
-          ),
-        ],
-      ),
-      body: Column(
-        children: [
-          Stack(
-            // fit: StackFit.expand,
-            children: [
-              const ListTile(),
-              Align(
-                alignment: Alignment.bottomCenter,
-                child: TextField(
-                  controller: _textController,
-                  focusNode: FocusNode(),
-                  autofocus: true,
+    return StreamBuilder(
+        stream: FirebaseFirestore.instance
+            .collection('comments')
+            .where('postId', isEqualTo: widget.postId)
+            .orderBy('datePublished', descending: true)
+            .snapshots(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Scaffold(
+              body: CircularProgressIndicator(),
+            );
+          }
+
+          return Scaffold(
+            resizeToAvoidBottomInset: true,
+            appBar: AppBar(
+              title: const Text('Comments'),
+              actions: [
+                TextButton(
+                  onPressed: () async {
+                    await FireStoreMethod()
+                        .storeComment(widget.postId, _textController.text);
+                    _textController.text = '';
+                  },
+                  child: const Text(
+                    'Reply',
+                    style: TextStyle(fontSize: 16),
+                  ),
                 ),
+              ],
+            ),
+            body: GestureDetector(
+              onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Expanded(
+                    child: ListView.builder(
+                      scrollDirection: Axis.vertical,
+                      shrinkWrap: true,
+                      itemCount: snapshot.data!.docs.length,
+                      itemBuilder: ((context, index) {
+                        return Card(
+                          color: Colors.blueGrey[600],
+                          child: Container(
+                            padding: const EdgeInsets.symmetric(vertical: 10),
+                            child: Row(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.all(10.0),
+                                  child: CircleAvatar(
+                                    radius: 20,
+                                    backgroundImage: NetworkImage(snapshot
+                                        .data!.docs[index]['userPhotoUrl']),
+                                  ),
+                                ),
+                                Expanded(
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        snapshot.data!.docs[index]['username'],
+                                        style: const TextStyle(
+                                          fontSize: 18,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                      Text(
+                                        snapshot.data!.docs[index]['content'],
+                                        style: const TextStyle(fontSize: 16),
+                                      ),
+                                      Text(
+                                        DateFormat.jm().add_yMMMMd().format(
+                                            snapshot.data!
+                                                .docs[index]['datePublished']
+                                                .toDate()),
+                                        style: const TextStyle(
+                                            fontWeight: FontWeight.w300),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                                IconButton(
+                                  onPressed: () {},
+                                  icon: const Icon(Icons.favorite_border),
+                                )
+                              ],
+                            ),
+                          ),
+                        );
+                      }),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextField(
+                      controller: _textController,
+                      focusNode: FocusNode(),
+                      autofocus: true,
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                  )
+                ],
               ),
-            ],
-          )
-        ],
-      ),
-    );
+            ),
+          );
+        });
   }
 }
